e83fae9a3fd3ada7aaffa18958e9bb0627980a0c
Mars Wong <marswong618@gmail.com>
2018-01-24 13:05:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ def speak_to_grandma(word)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif word == word.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
